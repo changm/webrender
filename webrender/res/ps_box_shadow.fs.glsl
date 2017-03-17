aca67c87dd6669004da717ee09dc0355f4271384
Mason Chang <mchang@mozilla.com>
2017-03-16 22:27:48 -0700

    broken

--- webrender/res/ps_box_shadow.fs.glsl
+++ webrender/res/ps_box_shadow.fs.glsl
@@ -5,8 +5,8 @@
 void main(void) {
     vec2 uv = min(vec2(1.0), vMirrorPoint - abs(vUv.xy - vMirrorPoint));
     uv = mix(vCacheUvRectCoords.xy, vCacheUvRectCoords.zw, uv);
-    //oFragColor = vColor * texture(sCache, vec3(uv, vUv.z));
+    oFragColor = vColor * texture(sCache, vec3(uv, vUv.z));
     //float result = vPatchDevice.x / 20.0;
-    oFragColor = vec4(vOrigin.y / 20.0, 0.0, 0.0, 1.0);
+    //oFragColor = vec4(vOrigin.y / 20.0, 0.0, 0.0, 1.0);
 
 }
